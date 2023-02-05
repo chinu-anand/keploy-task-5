e11462944472fa6b178dbfc5296a6504606f2744
Chinu Anand <chinuanand81@gmail.com>
2023-02-05 19:09:22 +0530

    update css for dynamic content size box
    Signed-off-by: Chinu Anand <chinuanand81@gmail.com>

--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -8,4 +8,19 @@ router.get('/movielist', async (req, res)=> {
     res.status(200).json(movies);
 });
 
+router.post('/movielist', async (req, res)=> {
+    const movie = new Movie({
+        name: req.body.name,
+        genre: req.body.genre,
+        rating: req.body.rating
+    })
+
+    try{
+        const newMovie = await movie.save();
+        res.status(201).json(newMovie);
+    }catch(err){
+        res.status(400).json(err.message);
+    }
+});
+
 module.exports = router;
